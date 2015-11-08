b026357aca1802bc7fae4a52448f60469fbed2e5
chawlann <chawlannn@gmail.com>
2015-11-08 15:47:33 +0800

    update volume.c

--- volumn.c
+++ volumn.c
@@ -3,6 +3,6 @@
 int main(){
 	int a, b, c, V, S;
 	scanf_s("%d %d %d", &a, &b, &c);
-	printf_s("V=%d\nS=%d\n", a*b*c, a*b * 2 + b*c * 2 + a*c * 2);
+	printf_s("V=%d\nS=%d\n", a * b * c, a * b * 2 + b*c * 2 + a*c * 2);
 	return 0;
 }
